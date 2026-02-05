bebfb2078fa26c1ad891cb9f3fe7dceec400b324
amol.g.takate@gmail.com <you@example.com>
2026-02-05 20:30:29 +0000

    tes43

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-amol-feb26"
+    bucket         = "tf-state-user2-feb26"
     key            = "lab2/terraform.tfstate"
     region         = "us-west-2"
     use_lockfile   = true
